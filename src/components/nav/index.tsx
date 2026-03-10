5c393bcfe15c4731d96b6cce4062b859013420f5
Jonathan Braaten Mjøs <jonathan@mjos.no>
2026-03-10 20:42:23 +0100

    ui: fix nav links

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,11 +1,41 @@
-import { ChartNoAxesColumn } from 'lucide-react'
+'use client'
+import { cn } from '@/utils/cn'
+import { ChartNoAxesColumn, LayoutDashboard } from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Nav() {
+  const pathName = usePathname()
+  const isAnalyticsActive = pathName === '/analytics'
+  const isDashboardActive = pathName === '/'
+
   return (
     <nav>
-      <ul>
-        <li className="text-xs bg-accent-purple/20  rounded-md border border-accent-purple/40">
+      <ul className="flex gap-2">
+        <li
+          className={cn(
+            'text-xs rounded-md border transition-all transition-50',
+            isDashboardActive
+              ? 'bg-accent-blue/40 border-accent-blue/60'
+              : 'bg-accent-blue/20 border-accent-blue/40 hover:bg-accent-blue/30 hover:border-accent-blue/50'
+          )}
+        >
+          <Link
+            href="/"
+            className="flex items-center gap-2 uppercase text-accent-blue py-1 px-3"
+          >
+            <LayoutDashboard size={12} />
+            <span>dashboard</span>
+          </Link>
+        </li>
+        <li
+          className={cn(
+            'text-xs rounded-md border transition-all transition-50',
+            isAnalyticsActive
+              ? 'bg-accent-purple/40 border-accent-purple/60'
+              : 'bg-accent-purple/20 border-accent-purple/40 hover:bg-accent-purple/30 hover:border-accent-purple/50'
+          )}
+        >
           <Link
             href="/analytics"
             className="flex items-center gap-2 uppercase text-accent-purple py-1 px-3"
